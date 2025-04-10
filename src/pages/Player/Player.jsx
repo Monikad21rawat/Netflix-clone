37cc7c25956fe36cc2128ca083ddeda152184b02
Monikad21rawat <155979985+Monikad21rawat@users.noreply.github.com>
2025-04-10 13:35:34 +0530

    Update Player.jsx

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
@@ -44,50 +44,5 @@ const [apiData,setApiData]=useState({
 }
 
 export default Player
-=======
-import React, { useEffect, useState } from 'react'
-import './Player.css'
-import back_arrow_icon from '../../assets/back_arrow_icon.png'
-import { useNavigate, useParams } from 'react-router-dom'
 
-const Player = () => {
-  const {id}=useParams();
-  const navigate= useNavigate();
 
-const [apiData,setApiData]=useState({
-  name:"",
-  key: "",
-  published_at: "",
-  typeof: "",
-})
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODQzY2MzYzZhNTM1MTQ3NDAwNDYyYWIwNDEwMGYyNyIsIm5iZiI6MTcyMTA2MDI4OS42OTkzMSwic3ViIjoiNjY5Mzk0NzBkZTljNzEyNjZkYWQ0NjI0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.hXB_gJh4kcjZ8WfM7TWe8u5YLtAbaPMvUR5OBtgMg2E'
-    }
-  };
-  
- useEffect(()=>{
-  fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
-  .then(response => response.json())
-  .then(response =>setApiData(response.results[0]))
-  .catch(err => console.error(err));
- },[])
-
-  return (
-    <div className='player'>
-      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}} />
-      <iframe width='90%' height="90%" src={`https://www.youtube.com/embed/${apiData.key}`}
-        title='trailer' frameBorder='0' allowFullScreen ></iframe>
-      <div className="player-info">
-        <p>{apiData.published_at.slice(0,10)}</p>
-        <p>{apiData.name}</p>
-        <p>{apiData.type}</p>
-      </div>
-    </div>
-  )
-}
-
-export default Player
->>>>>>> 8f375c6d110d50da204284cd4c353c76935e419b
